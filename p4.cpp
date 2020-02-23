403f0bb68c00157d2e34a83032c12ecb643ca6cd
ozabell <kidlatmc@gmail.com>
2020-02-22 16:29:07 -0800

    Created createBoard() and working on displayBoard()

--- p4.cpp
+++ p4.cpp
@@ -24,8 +24,8 @@ const char SPACE = ' ';
 const char ASTERISK = '*';
 const char UNDERSCORE = '_';
 const int PERFECT_GAME = 6;
-const int ROW = 4;
-const int COL = 3;
+const int ROWS = 4;
+const int COLS = 3;
 const int BEGINNER_TIME = 10;
 const int EXPERT_TIME = 5;
 const string BOARD_A = /home/fac/sreeder/class/cs1420/p4data1.dat;
@@ -34,20 +34,29 @@ const int BOARD_CLEARED = 12;
 
 void welcome();
 
-char getDifficulty();
-// returns the difficulty the player wants for the game
+int getDifficulty();
+// returns the wait time based on the player's difficulty
 
-string createBoard();
-// this is where you pick the file
-// it feeds the chars into the string array
+bool createBoard(char board[][ROWS], string fileName);
+// returns true if the file worked and the board is full of chars
+// returns false if otherwise
 
-void displayBoard();
-//ya pass the 2d array here to print??
+void displayBoard(char board[][ROWS], int c1row = -1, int c1col =1,
+    int c2row = -1, int c2col = -1);
+// prints the board to screen with the astricks and underscores,
+// with one card flipped over and with both cards flipped over
 
 int takeTurn();
 // this is where the user is asked about the cards
 // wait() is called here
 // removeMatch() is called here too
+
+int pickFirstCard();
+// paras is the current board
+// returns row and col
+
+int pickSecondCard();
+// params is the current board AND the row and col of FIRST card
 
 void wait(int seconds);
 // pauses the program for x seconds
@@ -59,15 +68,16 @@ int main()
   int numOfTurns = 0;
   char ans = YES;
   int matchedPairs = 0;
-  char difficulty;
+  int waitTime;
+  char board[ROWS][COLS];
+  srand(time(0));
 
   welcome();
 
-  while(tolower(answ) == YES){
+  while(tolower(answ) == YES) {
       difficulty = getDifficulty();
       createBoard();
-
-      while(matchedPars > BOARD_CLEARED) {
+      while(matchedPairs > BOARD_CLEARED) {
         takeTurn();
       }
   }
@@ -84,28 +94,57 @@ void welcome()
   cout << endl << endl;
 }
 
-char getDifficulty()
+int getDifficulty()
 {
   char choice;
+  int time;
   cout << "There are two difficulties for this game. Please choose one.";
   cout << endl << "Beginner (b) or expert (e): "
   cin >> choice;
 
   if(tolower(choice) == EXPERT_TIME) {
-    return EXPERT_TIME;
+    time = EXPERT_TIME;
+  } else {
+  time = BEGINNER_TIME;
+  }
+  return time;
+}
+
+bool createBoard(char board[][COLS])
+{
+  string fileName;
+  int ranNum = rand() % ((20 - 10) + 1) + 10;
+  if((randNum % 2) == 0) { // file is chosen based on even or odd rand num
+    fileName = BOARD_A;
+  } else {
+    fileName = BOARD_B;
+  }
+  ifStreaminFile;
+
+  inFile.open(fileName);
+  if(inFile.fail()) {
+    return false;
   }
 
-  return BEGINNER_TIME;
+  for(int r = 0; r < ROWS; r++) {
+    for(int c = 0; c < COLS; c++) {
+      inFile >> board[r][c];
+    }
+  }
+  inFile.close();
+  return true;
 }
 
-string createBoard()
+void displayBoard(char board[][ROWS], int c1row = -1, int c1col =1,
+    int c2row = -1, int c2col = -1)
 {
+  for(int i = 0; i < ROWS; i++) {
+    cout << SPACE << SPACE << SPACE << i;
+  }
 
-}
+  for(int row = 0; row < ROW; row++) {
 
-void displayBoard()
-{
-
+  }
 }
 
 int takeTurn()
