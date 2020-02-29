880637050a146818e27ad549e02f99ad56dd337b
ozabell <kidlatmc@gmail.com>
2020-02-29 13:14:50 -0800

    starting p5.cpp, creating struct for parts and file reading

---
+++ p5.cpp
@@ -0,0 +1,29 @@
+// Isabel Ovalles
+// 2-29-2020
+// p5.cpp
+
+// Purpose: This program serves as an inventory for the store
+//
+// Input:
+//
+// Processing:
+//
+// Output:
+//
+// Example:
+
+#include<iostream>
+#include <fsteam>
+#include <iomanip>
+
+using namespace std;
+
+struct Part {
+
+
+};
+
+int main()
+{
+  return 0;
+}
