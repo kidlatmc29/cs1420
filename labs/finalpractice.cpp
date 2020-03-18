8e35fdb9105dd4a5ffc082e7aa0b9effdca5fa6d
ozabell <kidlatmc@gmail.com>
2020-03-17 18:54:22 -0700

    adding outlines of fxns, wrote print and readFile

--- labs/finalpractice.cpp
+++ labs/finalpractice.cpp
@@ -33,11 +33,81 @@ struct Book {
   string title;
   string author;
   unsigned int totalPages;
+  unsigned int year;
 };
+
+const int MAX = 10;
+const string DATA = "finalpracticeinput.dat";
+
+void welcome();
+
+int readFile(Book list[]);
+
+void printBook(Book list[], int index);
+
+void goodbye();
 
 int main()
 {
+  Book list[MAX];
+  int numOfBooks = 0;
 
+  numOfBooks = readFile(list);
 
+  if(numOfBooks < 1) {
+    cout << "Problem with file. Press [ENTER] to quit the program...";
+    cout.get();
+    goodbye();
+    return 0;
+  }
+
+  cout << "List of books in Isabel's bookshelf: " << endl;
+  for(int index = 0; index < numOfBooks; index++) {
+    printBook(list, index);
+  }
+
+  goodbye();
   return 0;
+}
+
+void welcome()
+{
+  cout << endl << endl;
+  cout << "Welcome to Isabel's library!" << endl << endl;
+}
+
+int readFile(Book list[], int numOfBooks)
+{
+  ifstream inFile;
+  string fileName = DATA;
+  int count = 0;
+
+  cout << "Reading data from " << fileName "....." << endl;
+  inFile.open(fileName);
+
+  if(!inFile.file()) {
+    while(count < MAX && inFile >> list[count].title) {
+      inFile >> list[count].author
+             >> list[count].totalPages;
+      count++;
+    }
+  } else {
+    count = 0;
+  }
+  inFile.close();
+  return count;
+}
+
+void printBook(Book list[], int index)
+{
+  cout << "Title: " << list[index].title << endl
+       << "Author: " << list[index].author << endl
+       << "Publish Year: " << list[index].year << endl
+       << "Page Count: " << list[index].totalPages << endl
+       << endl;
+}
+
+void goodbye()
+{
+  cout << "Goodbye! Please come again!" << endl << endl;
 }
